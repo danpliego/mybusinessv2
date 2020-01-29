380dbf47068bcf795fc6a3bfca86a1dd6f95a95e
El dano <danielpliego@gmail.com>
2020-01-29 11:06:47 -0600

    restore all product page

--- src/pages/mybusiness/index.js
+++ src/pages/mybusiness/index.js
@@ -36,61 +36,64 @@ const IndexPage = () => (
       </Comp.Width75>
     </Comp.Container>
     <ProductBlueArea>
-      <Comp.FlexCenterChild>
-        <CircleContainer>
-          <img src={Circle1} />
-          <div>
-            <p className="extended">Ventas</p>
-            <p className="extended">Compras</p>
-            <p className="extended">Control de inventario</p>
-          </div>
-        </CircleContainer>
-        <CircleContainer>
-          <img src={Circle2} />
-          <div>
-            <p className="extended">Clientes</p>
-            <p className="extended">Cuentas por cobrar</p>
-            <p className="extended">Cuentas por pagar</p>
-          </div>
-        </CircleContainer>
-        <CircleContainer>
-          <img src={Circle1} />
-          <div>
-            <p className="extended">Facturación electronica</p>
-            <p className="extended">Pago de servicios</p>
-            <p className="extended">Proveedores</p>
-          </div>
-        </CircleContainer>
-        <CircleContainer>
-          <img src={Circle2} />
-          <div>
-            <p className="extended">Programación</p>
-            <p className="extended">Venta de recargas electrónicas</p>
-            <p className="extended">y más...</p>
-            <span className="body-l font-sm">
-              *(se adapta a tu giro de negocio)
-            </span>
-          </div>
-        </CircleContainer>
-      </Comp.FlexCenterChild>
-      <Comp.Width75>
-        <h2 className="extended text-center">Características</h2>
-        <ul className="font-xl">
-          <li>Licencia por máquina.</li>
-          <li>La licencia solo se paga una vez.</li>
-          <li>
-            Se puede instalar a partir de Windows 7 service pack 1 hasta Windows
-            10.
-          </li>
-          <li>
-            Puedes cambiar a la versión más reciente pagando una actualización.
-          </li>
-        </ul>
-      </Comp.Width75>
-      <Comp.Width50>
-        <h2 className="extended text-center">Descarga la prueba grátis</h2>
-        <MyForm />
-      </Comp.Width50>
+      <Comp.Container>
+        <Comp.FlexCenterChild>
+          <CircleContainer>
+            <img src={Circle1} />
+            <div>
+              <p className="extended">Ventas</p>
+              <p className="extended">Compras</p>
+              <p className="extended">Control de inventario</p>
+            </div>
+          </CircleContainer>
+          <CircleContainer>
+            <img src={Circle2} />
+            <div>
+              <p className="extended">Clientes</p>
+              <p className="extended">Cuentas por cobrar</p>
+              <p className="extended">Cuentas por pagar</p>
+            </div>
+          </CircleContainer>
+          <CircleContainer>
+            <img src={Circle1} />
+            <div>
+              <p className="extended">Facturación electronica</p>
+              <p className="extended">Pago de servicios</p>
+              <p className="extended">Proveedores</p>
+            </div>
+          </CircleContainer>
+          <CircleContainer>
+            <img src={Circle2} />
+            <div>
+              <p className="extended">Programación</p>
+              <p className="extended">Venta de recargas electrónicas</p>
+              <p className="extended">y más...</p>
+              <span className="body-l font-sm">
+                *(se adapta a tu giro de negocio)
+              </span>
+            </div>
+          </CircleContainer>
+        </Comp.FlexCenterChild>
+        <Comp.Width75>
+          <h2 className="extended text-center">Características</h2>
+          <ul className="font-xl">
+            <li>Licencia por máquina.</li>
+            <li>La licencia solo se paga una vez.</li>
+            <li>
+              Se puede instalar a partir de Windows 7 service pack 1 hasta
+              Windows 10.
+            </li>
+            <li>
+              Puedes cambiar a la versión más reciente pagando una
+              actualización.
+            </li>
+          </ul>
+        </Comp.Width75>
+        <Comp.Width50>
+          <h2 className="extended text-center">Descarga la prueba grátis</h2>
+          <MyForm />
+        </Comp.Width50>
+      </Comp.Container>
     </ProductBlueArea>
     <BannerContainer>
       <img src={HeroImage} />
@@ -136,5 +139,10 @@ export const CircleContainer = styled.div`
     justify-content: center;
     color: white;
     font-size: 1.5rem;
+  }
+
+  span {
+    position: absolute;
+    bottom: -3rem;
   }
 `;
