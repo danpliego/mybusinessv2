c8d5c6ee98174492d3792570b1a89a24ca62dc4f
El dano <danielpliego@gmail.com>
2020-01-30 14:37:17 -0600

    changes in cloud services

--- src/pages/productos/servicios-en-la-nube.js
+++ src/pages/productos/servicios-en-la-nube.js
@@ -28,39 +28,51 @@ const IndexPage = () => (
     </Comp.BannerContainer>
     <Comp.Container>
       <Comp.Width75>
-        <p>
+        <p className="font-lg">
           Los servicios en la nube de MyBusiness POS te permiten subir toda la
           información de tu negocio a un servidor online seguro. Desde aquí
           puedes acceder a tus ventas en tiempo real, controlar tus inventarios,
           cambiar precios, hacer respaldo de datos, obtener reportes de la
           operación de tus sucursales y hacer ventas en ruta.
         </p>
-        <p>
+        <p className="font-lg">
           Y lo mejor de todo, es que puedes controlarlo desde tu celular gracias
-          a nuestra aplicación Pos Tools.{" "}
+          a nuestra aplicación Pos Tools.
         </p>
+        <hr />
+        <h3 className="text-center extended">
+          ¿Cómo empiezo a usar la nube de MyBusiness?{" "}
+        </h3>
         <p>
-          Para comenzar a usar este servicio lo primero que debes hacer es
-          <strong>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://postools.azurewebsites.net/registrarse.aspx"
-            >
-              {" "}
-              registrarte aquí
-            </a>
-          </strong>
-          . Nuestro equipo se contactará contigo para darte seguimiento.
-        </p>
-        <p className="body-l font-xs">
-          *En este portal también puedes configurar los accesos a los módulos de
-          tu punto de venta y establecer que equipos pueden subir o bajar
-          información.{" "}
-        </p>
-        <p>
-          Una vez dado de alta, descarga Pos Tools y descubre lo fácil que es
-          operar tu negocio desde tu celular.{" "}
+          <ol className="font-lg">
+            <li>
+              Lo primero que debes hacer es{" "}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                href="https://postools.azurewebsites.net/registrarse.aspx"
+              >
+                {" "}
+                registrarte aquí
+              </a>
+              *. Nuestro equipo se contactará contigo para darte seguimiento.{" "}
+              <br />
+              <br />
+              <span className="font-sm body-l">
+                *En este portal también puedes configurar los accesos a los
+                módulos de tu punto de venta y establecer que equipos pueden
+                subir o bajar información.{" "}
+              </span>
+            </li>
+            <li>
+              Una vez dado de alta,{" "}
+              <a href="https://play.google.com/store/apps/details?id=postools.mybusinesspos.com">
+                descarga Pos Tools en google store
+              </a>{" "}
+              y entra a tu cuenta.
+            </li>
+            <li>¡Listo! Ahora tienes acceso a tu negocio desde tu celular.</li>
+          </ol>
         </p>
       </Comp.Width75>
     </Comp.Container>
