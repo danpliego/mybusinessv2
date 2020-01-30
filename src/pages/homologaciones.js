c2565669d425970c23723b7a8b994c1c676de674
El dano <danielpliego@gmail.com>
2020-01-30 12:59:14 -0600

    homologaciones

--- src/pages/homologaciones.js
+++ src/pages/homologaciones.js
@@ -3,13 +3,31 @@ import React from "react";
 import Layout from "../components/layout";
 import * as Comp from "../components/layoutComponents";
 import SEO from "../components/seo";
+import SectionTitle from "../components/sectionTitle";
+
+import HeroImage from "../images/soporte/hero-image.jpg";
 
 const IndexPage = () => (
   <Layout>
-    <Comp.Container>
-      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-      homologaciones
-    </Comp.Container>
+    <SEO title="Homologaciones" keywords={[`gatsby`, `application`, `react`]} />
+    <SectionTitle name="Homologaciones" />
+    <Comp.BannerContainer>
+      <Comp.BannerImage>
+        <img src={HeroImage} />
+      </Comp.BannerImage>
+      <Comp.Container>
+        <Comp.BannerBox>
+          <Comp.Width75>
+            <iframe
+              src="https://www.mybusinesspos.com/homologaciones.aspx?wmode=transparent"
+              width="100%"
+              height="300"
+              frameBorder="0"
+            ></iframe>
+          </Comp.Width75>
+        </Comp.BannerBox>
+      </Comp.Container>
+    </Comp.BannerContainer>
   </Layout>
 );
 
