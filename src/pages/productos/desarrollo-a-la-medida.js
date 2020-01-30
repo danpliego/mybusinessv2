7388b277cd3e81dcf3e8e003a576104f44264655
El dano <danielpliego@gmail.com>
2020-01-30 12:55:21 -0600

    add desarrollo a la medida

--- src/pages/productos/desarrollo-a-la-medida.js
+++ src/pages/productos/desarrollo-a-la-medida.js
@@ -2,13 +2,12 @@
 import React from "react";
 
 import Layout from "../../components/layout";
-import * as Comp from "../../components/layoutComponents";
-import SEO from "../../components/seo";
+import SectionTitle from "../../components/sectionTitle";
 
 import HeroImage from "../../images/desarrollo-a-la-medida/hero-image.jpg";
-import SectionTitle from "../../components/sectionTitle";
-import BoletinBanner from "../../components/boletinBanner";
-
+import * as Components from "../../components/layoutComponents";
+import SEO from "../../components/seo";
+import ContactForm from "../../components/contact_form";
 const IndexPage = () => (
   <Layout>
     <SEO
@@ -16,15 +15,26 @@ const IndexPage = () => (
       keywords={[`gatsby`, `application`, `react`]}
     />
     <SectionTitle name="Desarrollo a la medida" />
-    <Comp.BannerContainer>
-      <Comp.BannerImage>
+    <Components.BannerContainer>
+      <Components.BannerImage>
         <img src={HeroImage} />
-      </Comp.BannerImage>
-      <Comp.BannerContent>
-        <h1 className="extended-medium">contenido here...</h1>
-      </Comp.BannerContent>
-    </Comp.BannerContainer>
-    <BoletinBanner />
+      </Components.BannerImage>
+      <Components.Container>
+        <Components.BannerBox>
+          <Components.Width50>
+            <p>
+              Tu negocio merece un sistema de punto de venta hecho completamente
+              a la medida, por eso MyBusiness POS es el único software que puede
+              adecuarse a los requerimientos de tu giro comercial. <br />
+              <br />
+              Si requieres más información déjanos tus datos y haznos saber que
+              te gustaría modificar en el sistema.
+            </p>
+            <ContactForm />
+          </Components.Width50>
+        </Components.BannerBox>
+      </Components.Container>
+    </Components.BannerContainer>
   </Layout>
 );
 
