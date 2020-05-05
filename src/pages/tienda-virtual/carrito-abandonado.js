c1a605efd1450cba182c55bb61ac16a3213888e4
El dano <danielpliego@gmail.com>
2020-05-05 18:12:06 -0500

    add pages

---
+++ src/pages/tienda-virtual/carrito-abandonado.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line
+import React from "react";
+
+import Layout from "../../components/layout";
+
+import * as Components from "../../components/layoutComponents";
+import SectionTitle from "../../components/sectionTitle";
+import SEO from "../../components/seo";
+
+import HeroImage from "../../images/mybusiness/hero-image.jpg";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Tienda Virtual" keywords={[`gatsby`, `application`, `react`]} />
+    <SectionTitle name="Tienda Virtual: MyBusiness POS" />
+    <Components.BannerContainer>
+      <Components.BannerImage>
+        <img src={HeroImage} />
+      </Components.BannerImage>
+      <Components.Container>
+        <Components.BannerBox>
+          <Components.Width75>
+            <h4 style={{ textAlign: "center" }}>Carrito abandonado</h4>
+            <p>texto aqui</p>
+          </Components.Width75>
+        </Components.BannerBox>
+      </Components.Container>
+    </Components.BannerContainer>
+  </Layout>
+);
+
+export default IndexPage;
